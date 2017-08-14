f51d0038ce5a075d2f170c7d52a3fed6af738871
Valentin Popov <info@valentineus.link>
2017-08-14 15:57:39 +0400

    Minor fixes to upload the plugin in Moodle repository.

--- version.php
+++ version.php
@@ -8,6 +8,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+$plugin->release   = '0.0.1 (Build: 2017081400)';
 $plugin->version   = 2017081400;
-$plugin->requires  = 2017050500;
+$plugin->requires  = 2016112900;
 $plugin->component = 'auth_http';
+$plugin->maturity  = MATURITY_RC;
